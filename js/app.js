1fa6ab8583f4cc6cb98ccc6cf8f40160ee3978f7
Travis <Travis@Traviss-MacBook-Pro.local>
2016-11-30 17:44:58 -0500

    changed folder structure

---
+++ js/app.js
@@ -0,0 +1,30 @@
+var app = angular.module('musicMod', ['ngRoute']);
+
+app.config(function($routeProvider){
+
+  $routeProvider
+  .when('/', {
+    controller: 'mainCtrl',
+    templateUrl: 'partials/main.html'
+  })
+
+  .when('/rock', {
+    controller: 'rockCtrl',
+    templateUrl: 'partials/rock.html'
+  })
+
+  .when('/pop', {
+    controller: 'popCtrl',
+    templateUrl: 'partials/pop.html'
+  })
+
+  .when('/country', {
+    controller: 'countryCtrl',
+    templateUrl: 'partials/country.html'
+  })
+
+  .when('/rap', {
+    controller: 'rapCtrl',
+    templateUrl: 'partials/rap.html'
+  });
+});
