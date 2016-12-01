ee06f06de220b450a56e776b9eb49a36a46d4e44
Gaoia Vang <nkauj13@gmail.com>
2016-11-30 20:10:49 -0500

    first gao commit

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+
+gulp.task('default', function() {
+
+});
+
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("sass/*.scss", ['sass']);
+    gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+gulp.task('sass', function() {
+    return gulp.src("sass/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("./"))
+        .pipe(browserSync.stream());
+});
